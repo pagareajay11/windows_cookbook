0c7bbb2c823556de56bd4199d4454b20816a4728
pagareajay11 <57344952+pagareajay11@users.noreply.github.com>
2022-10-14 17:48:26 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 
 default['chrome']['msi'] = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BA4BFE3F5-F8BA-4F47-B5FC-F30FBAA10185%7D%26lang%3Den-GB%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26brand%3DCHBD%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe'
-default['install-packages']['vscode'] = 'y'
+
